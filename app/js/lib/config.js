455ff31029796ddb0db4a73908ad25a79f7054dd
Alex <ipritoflex@yandex.ru>
2016-08-13 14:27:32 +0300

    Introducing new version 0.2.8

--- app/js/lib/config.js
+++ app/js/lib/config.js
@@ -3,7 +3,7 @@ var Config = window.Config = window.Config || {};
 Config.App = {
     id: 216,
     hash: '8da85b0d5bfe62527e5b244a309159c3',
-    version: '0.2.7',
+    version: '0.2.8',
     domains: [
         'localhost:3000',
         'acm.alexbelov.xyz'
